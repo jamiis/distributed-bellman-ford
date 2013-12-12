174a31e9c38caca744d50041528abc1554ac953f
Jamis Johnson <jamismanwaring@gmail.com>
2013-12-11 19:11:06 -0500

    define distributed bellman-ford update rule
    update_neighbor() updates the costs of a neighbor and then runs
    the bellman-ford algorithm, updating every cost to every node in
    the network.
    
    nodes holds all state information necessary for bellman-ford:
        1. direct distances to neighbors
        2. estimated cost from self to every node in the network
        3. neighbors' estimated costs to every node in the network
    
    an example of the nodes data structure is given by nodes_example and
    will be deleted a little down the road.
    
    adds helper functions for getting neighbors, getting the estimated
    costs to every node in the network, and getting a formatted string
    address given an ip and port

--- bfclient.py
+++ bfclient.py
@@ -1,4 +1,6 @@
-import sys, socket, select
+import sys, socket
+from select import select
+from collections import defaultdict
 
 DEBUG = True
 if DEBUG:
@@ -6,12 +8,63 @@ if DEBUG:
 
 SIZE = 1024
 
+nodes_example = {
+    '127.0.0.0': {
+        'cost': 0.0,
+        'is_neighbor': False, # TODO not sure
+    },
+    '127.0.0.1': {
+        'cost': 10.0,
+        'is_neighbor': True,
+        # vals below are present iff is_neighbor
+        'direct' : 10.0,
+        'costs': { 
+            '127.0.0.0': 10.0,
+            '127.0.0.2': 5.0,
+            '127.0.0.3': 15.0,
+        }
+    },
+    '127.0.0.2': {
+        'cost': 15.0,
+        'is_neighbor': True,
+        'direct' : 100.0,
+        'costs': { 
+            '127.0.0.0': 16.0,
+            '127.0.0.1': 5.0,
+            '127.0.0.3': 1.0,
+        }
+    },
+    '127.0.0.3': {
+        'cost': 16.0, # 0 -> 1 -> 2 -> 3
+        'is_neighbor': False,
+        'direct': float("inf") # ? 
+    },
+}
+
+
+def update_neighbor(addr, costs):
+    """ update costs given costs received from a neighboring node """
+    # will help with debugging bc this should never happen
+    assert nodes[addr]['is_neighbor']
+    # first, save neighbor's new dist vector
+    nodes[addr]['costs'] = costs
+    # update every node's cost
+    for no in nodes.keys():
+        mini = float("inf")
+        neighbors = get_neighbors() 
+        for ne in neighbors.keys():
+            # dist = direct cost to neighbor + cost from node to neighbor
+            dist = neighbors[ne]['direct'] + nodes[no]['costs'][ne]
+            if dist < mini:
+                mini = dist
+        nodes[no]['cost'] = dist
+
 def join_network():
     """ add node to the distributed bellman ford algorithm network """
     inputs = [sock, sys.stdin]
     running = True
     while running:
-        in_ready, out_ready, except_ready = select.select(inputs,[],[]) 
+        in_ready, out_ready, except_ready = select(inputs,[],[]) 
         for s in in_ready:
             if s == sys.stdin:
                 print 'stdin: {0}'.format(sys.stdin.readline())
@@ -40,15 +93,42 @@ def parse_args(args):
     timeout = float(args.pop(0))
     neighbors = {}
     while len(args):
-        # neighbors[address] = distance
-        neighbors[args[0]+":"+args[1]] = float(args[2])
+        address = get_addr(ip=args[0], port=args[1])
+        distance = float(args[2])
+        neighbors[address] = node(cost=distance, direct=distance, is_neighbor=True)
         del args[0:3]
-    if DEBUG:
-        print "neighbors: "
-        pprint(neighbors)
     return port, timeout, neighbors
 
+def node(cost, direct, is_neighbor=False, costs=None):
+    """ centralizes the pattern for creating new nodes """
+    if costs == None:
+        costs = defaultdict(lambda c: float("inf"))
+    return {
+        'cost'       : cost,
+        'direct'     : direct,
+        'is_neighbor': is_neighbor,
+        'costs'      : defaultdict(lambda c: float("inf")),
+    }
+
+def get_addr(ip, port):
+    return "{ip}:{port}".format(ip=ip, port=port)
+
+def get_neighbors():
+    """ return dict of all neighbors (does not include self) """
+    return dict([d for d in nodes.items() if d[1]['is_neighbor']])
+
+def get_costs():
+    """ return dict mapping nodes to costs; returns own distance vector """
+    return dict([ (no[0], no[1]['cost']) for no in nodes.items()] )
+
 if __name__ == '__main__':
-    port, timeout, neighbors = parse_args(sys.argv[1:])
+    port, timeout, nodes = parse_args(sys.argv[1:])
     sock = setup_server()
+    # set distance to self to 0
+    me = get_addr(*sock.getsockname())
+    nodes[me] = { 'cost': 0.0, 'direct': 0.0, 'is_neighbor': False }
+    if DEBUG:
+        print "nodes: "
+        pprint(nodes)
+    # add node to network
     join_network()
